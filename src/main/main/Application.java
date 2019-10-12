0fb9dbaf16cd7dd6f9956f56aa5564dd64307eb5
michael.rempel@gmx.net <michael.rempel@gmx.net>
2019-10-12 23:29:59 +0200

    init methods and testing

--- src/main/main/Application.java
+++ src/main/main/Application.java
@@ -3,13 +3,20 @@ package main;
 import BaggageScanner.BaggageScanner;
 import Employees.InspectorManualPostControl;
 import Employees.*;
+import Passenger_Baggage.Gender;
+import Passenger_Baggage.HandBaggage;
+import Passenger_Baggage.Passenger;
 import configuration.Configuration;
+import FileReader.*;
 
-import java.net.PasswordAuthentication;
+import java.io.IOException;
+import java.util.*;
 
 public class Application {
 
     public static void main(String[] args) {
+
+        String AESkey = "Hallo Welt";
 
         //hallosad
         // Festlegung dea Algorithmus zur such nach verboten Gegenstaende
@@ -18,9 +25,19 @@ public class Application {
         // Gepäkscännner legt seiene Objekte an
         BaggageScanner BS1=new BaggageScanner();
 
+        Map<Integer, Passenger> passengers = new HashMap<>();
+
+        initPassengers(passengers);
+        initAssignments(passengers);
+        initPassengerBaggage(passengers.get(1));
+
+        HandBaggage testBag = passengers.get(1).getBaggageByIndex(0);
+
+        System.out.println(testBag.getLayer(4).getContent().substring(0,50));
 
 
-        //InspectorManualPostControl insMPC=new InspectorManualPostControl(1, "Mueller", "10.10.1990", true, BS1.setManualPostControl(););
+        iInspectorManualPostControl inspManCtrl= new InspectorManualPostControl(1, "Mueller", "10.10.1990",false);
+        //scanner.registerInspectorManualContr(inspManCtrl);
         //read passanger data -> 2 Maps:
         //map id - passagierObject
         //map passangerID - handgepäckID
@@ -36,6 +53,107 @@ public class Application {
         //ON state
 
     }
+
+    private static void initPassengerBaggage(Passenger passenger) {
+        int[] bagIDs = passenger.getBaggageIDs();
+        for (int i=0;i<bagIDs.length;i++){
+            IFileReader fRead = new BaggageScannerFileReader();
+            int passID;
+            int[] baggageIDs = new int[3];
+            int numBaggage;
+
+            try {
+                String baggage_filePath = "data/hand_baggage_"+bagIDs[i]+".txt";
+                String[] baggage_content = fRead.readFileToString(baggage_filePath);
+                    HandBaggage bag = new HandBaggage(baggage_content[0]);
+                    passenger.addHandBaggage(bag);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static void initAssignments(Map<Integer, Passenger> passengers) {
+        IFileReader fRead = new BaggageScannerFileReader();
+        int passID;
+        int[] baggageIDs = new int[3];
+        int numBaggage;
+
+        String[] assignments;
+        try {
+            assignments = fRead.readFileToString("data/assignment.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            for (String assignment : assignments
+            ) {
+                //  // passenger_id;number_of_hand_baggage;hand_baggage_id(s)
+                if (assignment.contains("//")) continue;
+                String[] subStrs = assignment.split("[,;]");
+                passID = Integer.valueOf(subStrs[0]);
+                numBaggage = Integer.valueOf(subStrs[1]);
+                Arrays.fill(baggageIDs, 0);
+                for (int i = 0; i < numBaggage; i++) {
+                    baggageIDs[i] = Integer.valueOf(subStrs[i + 2]);
+                }
+                if (passengers.containsKey(passID)) {
+                    passengers.get(passID).setBaggageIDs(baggageIDs);
+                }
+            }
+        } catch (NumberFormatException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+
+    private static void initPassengers(Map<Integer, Passenger> passengers) {
+        IFileReader fRead = new BaggageScannerFileReader();
+
+        String[] passSubStrs;
+        int passID;
+        String name;
+        Gender gender;
+        String birthdate;
+        String street;
+        String city;
+        String plz;
+        String passport_id;
+
+        try {
+            String[] passengerStrings = fRead.readFileToString("data/passenger.txt");
+            // id,name,gender,birthdate,address,passport_id
+            for (String passengerString: passengerStrings
+            ) {
+                if (passengerString.contains("//")) continue;
+
+                passSubStrs = passengerString.split(",");
+                if(passSubStrs.length!=8){
+                    String id = "??";
+                    if (passSubStrs.length>0) id=passSubStrs[0];
+                    System.out.println("Faulty passenger data found at ID '"+id+"', discarding");
+                    continue;
+                }
+                passID = Integer.valueOf(passSubStrs[0]);
+                name = passSubStrs[1];
+                gender = passSubStrs[2].equalsIgnoreCase("male") ? Gender.male : Gender.female;
+                birthdate = passSubStrs[3];
+                street = passSubStrs[4];
+                city = passSubStrs[5];
+                plz = passSubStrs[6];
+                passport_id = passSubStrs[7];
+                passengers.put(passID, new Passenger(passID, name, gender, birthdate, street, city, plz, passport_id));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 
 }
