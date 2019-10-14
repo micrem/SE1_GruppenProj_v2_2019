1ee1b7aabdf2f6088a39c400394a596ceddbe271
michael.rempel@gmx.net <michael.rempel@gmx.net>
2019-10-14 14:00:47 +0200

    plasticTray interface

--- src/main/baggageScanner/IBaggageScannerStation.java
+++ src/main/baggageScanner/IBaggageScannerStation.java
@@ -1,6 +1,7 @@
 package baggageScanner;
 
 public interface IBaggageScannerStation {
+    PlasticTray removePlasticTray();
     PlasticTray getPlasticTray();
     void putPlasticTray(PlasticTray plasticTray);
 }
