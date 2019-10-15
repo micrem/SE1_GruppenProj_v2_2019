fdf7ca86d5b1d491eb8174cd8f68b6b1f3c0e889
michael.rempel@gmx.net <michael.rempel@gmx.net>
2019-10-15 23:47:13 +0200

    add ProfileType comments

--- src/main/cardReader/ProfileType.java
+++ src/main/cardReader/ProfileType.java
@@ -1,9 +1,22 @@
 package cardReader;
 
+/**
+ * k o - no scanner access
+ * I: 1234
+ * S: 5
+ * T: 6
+ *
+ * 1 MoveBeltForward
+ * 2 MoveBeltBackwards
+ * 3 Scan
+ * 4 Alarm
+ * 5 Report
+ * 6 Maintenance
+ */
 public enum ProfileType {
     K,
     O,
-    I,  //1 2 3 4
-    S,  //5
-    T   //6
+    I,
+    S,
+    T
 }
