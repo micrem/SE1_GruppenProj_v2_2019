c7f0795202854111082b0413ce259ba7a2c7fa8b
micpel <michael.rempel@gmx.net>
2019-10-15 14:11:32 +0200

    add card to supervisor

--- src/main/employees/Supervisor.java
+++ src/main/employees/Supervisor.java
@@ -2,16 +2,24 @@ package employees;
 
 
 import baggageScanner.WorkplaceSupervision;
+import cardReader.CardReader;
+import idCard.*;
+
+import java.util.Date;
 
 public class Supervisor extends Employee implements iSupervisor{
     private boolean isSenior;
     private boolean isExecutive;
+    private IIDCard idCard;
     WorkplaceSupervision assignedWorkplaceSupervision;
 
     public Supervisor(int pid, String pname, String pbirthDate, boolean pisSenior, boolean pisExecutive) {
         super(pid, pname, pbirthDate);
         isSenior=pisSenior;
         isExecutive=pisExecutive;
+        String validUntil = "2.2.2222";
+        idCard = new IDCard(pid, validUntil, CardType.staff);
+
     }
     @Override
     public void setAssignedWorkplaceSupervision(WorkplaceSupervision passignedWorkplaceSUpervision){
