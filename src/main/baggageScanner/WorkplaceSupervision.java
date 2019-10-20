eb92bdbb4ac432f6059d8f83b24fde9361d2af5a
michael.rempel@gmx.net <michael.rempel@gmx.net>
2019-10-20 17:03:25 +0200

    remove Station interface from WorkplaceStation

--- src/main/baggageScanner/WorkplaceSupervision.java
+++ src/main/baggageScanner/WorkplaceSupervision.java
@@ -1,33 +1,12 @@
 package baggageScanner;
 
-public class WorkplaceSupervision implements IBaggageScannerStation{
+public class WorkplaceSupervision {
     private IBaggageScanner baggageScanner;
-    private PlasticTray plasticTray;
+
 
     public  WorkplaceSupervision(IBaggageScanner baggageScanner){
         this.baggageScanner = baggageScanner;
         //baggageScanner.setWorkplaceSupervision(this);
     }
 
-    @Override
-    public PlasticTray peekPlasticTray() {
-        return plasticTray;
-    }
-
-    @Override
-    public IBaggageScanner getBaggageScanner() {
-        return baggageScanner;
-    }
-
-    @Override
-    public PlasticTray removePlasticTray() {
-        PlasticTray tempBaggage = this.plasticTray;
-        this.plasticTray = null;
-        return tempBaggage;
-    }
-
-    @Override
-    public void putPlasticTray(PlasticTray plasticTray) {
-        this.plasticTray = plasticTray;
-    }
 }
